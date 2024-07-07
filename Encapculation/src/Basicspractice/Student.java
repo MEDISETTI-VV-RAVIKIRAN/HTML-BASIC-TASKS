307122e9c24ecb254d55976eeba2051ba4bedf8a
MEDISETTI-VV-RAVIKIRAN <1ravikiranmedisetti1@gmail.com>
2024-07-07 12:16:49 +0530

    Add files via upload

---
+++ Encapculation/src/Basicspractice/Student.java
@@ -0,0 +1,24 @@
+package Basicspractice;
+
+public class Student {
+
+    private int studentRollNum ;
+
+    private String studentName ;
+
+    public int getStudentRollNum() {
+        return studentRollNum;
+    }
+
+    public void setStudentRollNum(int studentRollNum) {
+        this.studentRollNum = studentRollNum;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+}
---
+++ Encapculation/src/Management.java
@@ -0,0 +1,6 @@
+import Basicspractice.Student;
+
+public class Management {
+    Student S = new Student();
+
+}
